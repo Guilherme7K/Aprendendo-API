3b0c970479472bbe9cc98090e4d9fa2dae1542e8
GUILHERME SOUZA <yunggui77@gmail.com>
2025-04-08 01:58:44 -0300

    Melhorias de Segurança

--- src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,26 +1,52 @@
 package br.com.criandoapi.projeto.controller;
 
-import br.com.criandoapi.projeto.DAO.IUsuario;
+import br.com.criandoapi.projeto.repository.IUsuario;
 import br.com.criandoapi.projeto.model.Usuario;
+import br.com.criandoapi.projeto.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/usuarios")
 public class UsuarioController{
 
     @Autowired
-    private IUsuario dao;
+    private UsuarioService usuarioService;
+    public UsuarioController(UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
 
     @GetMapping
-    public List<Usuario> listaUsuarios(){
-        return (List<Usuario>) dao.findAll();
+    public ResponseEntity<List<Usuario>> listaUsuarios(){
+        return ResponseEntity.status(200).body(usuarioService.listarUsuario());
     }
     @PostMapping
-    public Usuario criarUsuario(@RequestBody Usuario usuario){
-        Usuario usuarioNovo = dao.save(usuario);
-        return usuarioNovo;
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.status(201).body(usuarioService.criarUsuario(usuario));
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> editarUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.status(200).body(usuarioService.editarUsuario(usuario));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> excluirUsuario(@PathVariable Integer id){
+        usuarioService.excluirUsuario(id);
+        return ResponseEntity.status(204).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Usuario> validarSenha(@RequestBody Usuario usuario){
+        Boolean valid = usuarioService.validarSenha(usuario);
+        if(!valid){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.status(200).build();
     }
 }
